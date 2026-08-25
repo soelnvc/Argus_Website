659e8b53a2a72dbdd219bca255f58aa5ac50a449
Siddhesh Goel <soelnvc@gmail.com>
2026-08-25 15:24:05 +0530

    feat: add ProgressiveBlur component and necessary utility classes to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SmoothScroll from "@/components/SmoothScroll";
+import ProgressiveBlur from "@/components/ProgressiveBlur";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <SmoothScroll>{children}</SmoothScroll>
+        <ProgressiveBlur
+          position="bottom"
+          height="60px"
+          className="fixed bottom-0 left-0 right-0 z-50"
+        />
       </body>
     </html>
   );
 }
+
